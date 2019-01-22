f6beb410532e8c19259040b45cd7e415b2701449
nalrabah <nalrabah@hotmail.com>
2019-01-22 20:37:01 +0300

    done

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django import http
 # Create your views here.
+def taskfile(request):
+    context = {
+    "msg":"Hello World!",
+    }
+    return render(request, 'myfile.html', context)
